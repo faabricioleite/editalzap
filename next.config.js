40653e24cacb563bbbd5737115c421a4dd3d26ca
faabricioleite <leite8@gmail.com>
2025-04-11 22:32:00 -0300

    fix: remove output: export que estava causando falha no deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   reactStrictMode: false,
   images: {
     domains: ['editalzap.com.br', 'www.editalzap.com.br'],
